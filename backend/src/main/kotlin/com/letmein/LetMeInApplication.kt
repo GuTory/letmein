db41c808712be0dc3a9f5f03ac7087d43dd32b8d
I577305 <zmgtothkr@gmail.com>
2023-03-10 17:12:45 +0100

    spring init

---
+++ backend/src/main/kotlin/com/letmein/LetMeInApplication.kt
@@ -0,0 +1,11 @@
+package com.letmein
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LetMeInApplication
+
+fun main(args: Array<String>) {
+	runApplication<LetMeInApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/letmein/LetMeInApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.letmein
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LetMeInApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
