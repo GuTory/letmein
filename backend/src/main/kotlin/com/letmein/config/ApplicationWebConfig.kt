d49dd1b444e7f8366aec0c069797590a225f41ae
I577305 <zmgtothkr@gmail.com>
2023-03-21 16:21:26 +0100

    Update ApplicationWebConfig.kt

--- backend/src/main/kotlin/com/letmein/config/ApplicationWebConfig.kt
+++ backend/src/main/kotlin/com/letmein/config/ApplicationWebConfig.kt
@@ -6,17 +6,16 @@ import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import java.util.*
 
 
 @Configuration
 @EnableWebMvc
-class ApplicationConfig {
+class ApplicationWebConfig {
 
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource? {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = Arrays.asList("http://localhost:4200")
+        configuration.allowedOrigins = listOf("http://localhost:4200")
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", configuration)
         return source
