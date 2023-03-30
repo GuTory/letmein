96f7110e2f2597789e76086f99eb49d756e9aeee
Tóth Kristóf <tothkristof31@edu.bme.hu>
2023-03-30 11:40:06 +0200

    structure fixes and application component
    further to do:
     - styling
     - testing
     - microservices

--- frontend/src/app/components/application/application.component.ts
+++ frontend/src/app/components/application/application.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {ApplicationService} from "../../service/application/application.service";
+import {Application} from "../../model/application";
 
 @Component({
   selector: 'app-application',
@@ -8,8 +9,75 @@ import {ApplicationService} from "../../service/application/application.service"
 })
 export class ApplicationComponent {
 
+    applications: Application[] = [];
+
     constructor(private applicationService: ApplicationService) {
     }
 
+    saveApplication(application: Application): void {
+        this.applicationService.saveApplication(application);
+    }
 
+    updateApplication(application: Application): void {
+        this.applicationService.updateApplication(application);
+    }
+
+    deleteApplication(id: string): void {
+        this.applicationService.deleteApplication(id);
+    }
+
+    getApplications(): void {
+        this.applicationService.getApplications().subscribe(
+            {
+                next: (data) => this.applications = data,
+                error: (err: Error) => {
+                    console.log("Error getting applications: " + err);
+                }
+            }
+        );
+    }
+
+    getApplicationById(id: string): void {
+        this.applicationService.getApplicationById(id).subscribe({
+            next: data => {
+                this.applications = [data];
+            },
+            error: (err: Error) => {
+                console.log("Error getting application by id: " + err);
+            }
+        });
+    }
+
+    getAllApplicationsByEvent(event: string): void {
+        this.applicationService.getAllApplcationsByEvent(event).subscribe({
+            next: data => {
+                this.applications = data;
+            },
+            error: (err: Error) => {
+                console.log("Error getting applications by Event: " + err);
+            }
+        });
+    }
+
+    getAllApplicationsByUser(user: string): void {
+        this.applicationService.getAllApplicationsByUser(user).subscribe({
+            next: data => {
+                this.applications = data;
+            },
+            error: (err: Error) => {
+                console.log("Error getting applications by User: " + err);
+            }
+        });
+    }
+
+    getAllApplicationsByEventNameContains(eventName: string): void {
+        this.applicationService.getAllApplicationsByEventNameContains(eventName).subscribe({
+            next: data => {
+                this.applications = data;
+            },
+            error: (err: Error) => {
+                console.log("Error getting applications by Event Name: " + err);
+            }
+        });
+    }
 }
